68f1b88a1ec06308c0afafb510fe539e7e63de2a
Erick Adriano Nunes da Silva <61701606+ErickNunes97@users.noreply.github.com>
2022-03-04 22:49:25 -0300

    Add files via upload

---
+++ Area_do_Circulo/Area_do_Circulo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Area_do_Circulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ================================================");
+            Console.WriteLine("|                 Área do Círculo                |");
+            Console.WriteLine(" ================================================\n\n");
+
+
+            double pi = 3.1415;
+            Console.WriteLine("\nDigite o raio do círculo: ");
+            double r = double.Parse(Console.ReadLine());
+            double a = pi * (r * r);
+            Console.WriteLine("\nA área do seu círculo é: " + a);
+
+        }
+    }
+}
