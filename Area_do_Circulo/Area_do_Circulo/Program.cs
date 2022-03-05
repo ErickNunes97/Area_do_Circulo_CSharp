e57a5c98b6b87b477c82dac7ef5b4c33fec0728d
Erick Adriano Nunes da Silva <61701606+ErickNunes97@users.noreply.github.com>
2022-03-04 23:21:52 -0300

    Add files via upload

--- Area_do_Circulo/Area_do_Circulo/Program.cs
+++ Area_do_Circulo/Area_do_Circulo/Program.cs
@@ -6,17 +6,43 @@ namespace Area_do_Circulo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" ================================================");
-            Console.WriteLine("|                 Área do Círculo                |");
-            Console.WriteLine(" ================================================\n\n");
+            Console.WriteLine("\nDeseja calcular a área do círculo: ");
+
+            string a = Console.ReadLine();
+            Console.WriteLine("\n\n");
 
 
-            double pi = 3.1415;
-            Console.WriteLine("\nDigite o raio do círculo: ");
-            double r = double.Parse(Console.ReadLine());
-            double a = pi * (r * r);
-            Console.WriteLine("\nA área do seu círculo é: " + a);
+            while (a !="n")
+            {
+                
 
+                Console.WriteLine(" ************************************************");
+                Console.WriteLine(" *                 Área do Círculo              *");
+                Console.WriteLine(" ************************************************\n\n");
+
+
+                double pi = 3.1415;
+                Console.WriteLine("\nDigite o raio do círculo: ");
+                double r = double.Parse(Console.ReadLine());
+                double area = pi * (r * r);
+                Console.WriteLine("\nA área do seu círculo é: " + area);
+                Console.WriteLine("=====================================================\n");
+
+                Console.WriteLine("\nDeseja calcular novamente: ");
+                string n = Console.ReadLine();
+
+                if(n=="n")
+                {
+                    Console.WriteLine("\nFinalizado");
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("\nContinua\n\n");
+                    continue;
+                }
+            }
         }
     }
 }
